6e7a39c8d136c5455664a504ba878afb1a6363cc
milton-arg <milton.eduardo.sosa@gmail.com>
2020-12-03 13:17:57 +0100

    merge main into master

---
+++ leds/leds/test/test_leds.c
@@ -0,0 +1,63 @@
+#include "unity.h"
+#include "leds.h"
+
+
+uint16_t ledsVirtuales;
+
+
+void setUp()
+{
+    Leds_Create(&ledsVirtuales);
+}
+
+void test_LedsOffAfterCreate()
+{
+    uint16_t ledsVirtuales = 0xFFFF;
+    Leds_Create(&ledsVirtuales);
+    TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
+}
+
+void test_prender_led_individual(void)
+{
+    Leds_On(1);
+    TEST_ASSERT_EQUAL_HEX16(1, ledsVirtuales);
+}
+
+void test_apagar_led_individual(void)
+{
+    Leds_On(1);
+    Leds_Off(1);
+    TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
+}
+
+void test_prender_apagar_multiples(void)
+{
+    Leds_On(3);
+    Leds_On(4);
+    Leds_Off(3);
+    TEST_ASSERT_EQUAL_HEX16(1 << 3, ledsVirtuales);
+}
+
+void test_prender_todos(void)
+{
+    Leds_On_ALL();
+    TEST_ASSERT_EQUAL_HEX16(0xFFFF, ledsVirtuales);
+}
+
+void test_apagar_todos(void)
+{
+    Leds_On_ALL();
+    Leds_Off_ALL();
+    TEST_ASSERT_EQUAL_HEX16(0x0000, ledsVirtuales);
+}
+
+void test_consultar_estado_ON(void)
+{
+    Leds_On(4);
+    TEST_ASSERT_EQUAL_HEX16(1, estado_led(4));
+}
+
+void test_consultar_estado_OFF(void)
+{
+    TEST_ASSERT_EQUAL_HEX16(0, estado_led(4));
+}
