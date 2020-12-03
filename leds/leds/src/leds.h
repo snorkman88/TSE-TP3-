2d2c1777b06a58802fa206b28f2c7c51f444b23f
milton-arg <milton.eduardo.sosa@gmail.com>
2020-12-03 13:32:02 +0100

    borro archivos incorrectos

--- leds/leds/src/leds.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-void Leds_Create(uint16_t *puerto);
-void Leds_On(uint8_t led);
-void Leds_Off(uint8_t led);
-void Leds_On_ALL(void);
-void Leds_Off_ALL(void);
-uint8_t estado_led(uint8_t led);
